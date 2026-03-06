e0838e8a9bdbbeb5accaccb6063c4188ccb5ab07
Liangbai2333 <1739566851@qq.com>
2026-03-07 00:38:21 +0800

    fix

--- overrides/smart-convert.js
+++ overrides/smart-convert.js
@@ -510,9 +510,7 @@ function buildProxyGroups({
       name: PROXY_GROUPS.TRANSFER,
       icon: "https://gcore.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Proxy.png",
       type: "select",
-      "include-all": true,
-      filter: `^(?i)(?!.*${SELF_HOSTED_REGEX})`,
-      proxies: n,
+      proxies: n.filter((p) => !new RegExp(SELF_HOSTED_REGEX, "i").test(p)),
     },
   ].filter(Boolean);
 }
